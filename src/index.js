17bfe49f56e9cc61e9a018fdb2f489647048c529
KiriZhu <kiribaty88@gmail.com>
2020-10-14 15:25:15 +0200

    added mongodb connection and env file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = `mongodb+srv://kiribaty:${process.env.MONGOPASSWORD}@cluster0.eoapt.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to Mongo instance', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there');
